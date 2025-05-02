a5c79b25f9d343d8b9275179596769a7a27db038
Ryan Hedgecock <rhedgeco@gmail.com>
2025-05-01 20:19:11 -0700

    clean export structure for wayne macros

--- wayne/src/lib.rs
+++ wayne/src/lib.rs
@@ -1,7 +1,12 @@
+// re-export wayne_core under alias core
 pub mod core {
     pub use wayne_core::*;
 }
 
+// re-export wayne_protocol under alias protocol
 pub mod protocol {
     pub use wayne_protocol::*;
 }
+
+// re-export protocol macro at root level
+pub use protocol::protocol;

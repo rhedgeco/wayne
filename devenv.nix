924e02eb9c7941f741691491f12462f4ec06ee32
Ryan Hedgecock <rhedgeco@gmail.com>
2026-03-07 11:00:05 -0800

    simplify devenv file

--- devenv.nix
+++ devenv.nix
@@ -7,23 +7,4 @@
     enable = true;
     toolchainFile = ./rust-toolchain.toml;
   };
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from devenv
-  '';
-
-  # https://devenv.sh/basics/
-  enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
-  '';
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # See full reference at https://devenv.sh/reference/options/
 }
